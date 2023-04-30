1e73a4cb59df2c3808e48fc8c4d1793134c63d02
ngocnghien97 <ngoc1997307@gmail.com>
2023-04-30 22:03:43 +0700

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Ngọc Lê on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
